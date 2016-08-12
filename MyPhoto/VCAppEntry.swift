a61aaeeb8574d5627a7f4c59e7c4cd31849f7306
saumya <saumya@pivotaldesign.biz>
2016-08-12 15:53:53 +0530

    added photo selection

--- MyPhoto/VCAppEntry.swift
+++ MyPhoto/VCAppEntry.swift
@@ -9,13 +9,17 @@
 import Foundation
 import UIKit
 
-class VCAppEntry: UIViewController {
+class VCAppEntry: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet var imgSelected:UIImage!
     @IBOutlet var uiImgSelected:UIImageView!
     
+    let imagePicker:UIImagePickerController = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -23,10 +27,19 @@ class VCAppEntry: UIViewController {
 }
 
 extension VCAppEntry {
-    @IBAction func onSelectPhoto(){
+    @IBAction func onSelectPhoto(sender:UIButton){
         print("onSelectPhoto")
+        
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
     }
-    @IBAction func onUpload(){
+    @IBAction func onUpload(sender:UIButton){
         print("onUpload")
     }
 }
+
+extension VCAppEntry {
+}
+//
